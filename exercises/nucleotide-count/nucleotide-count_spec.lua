c8651f4dee623ee49e1f576764be0a3378d50827
Carlos Pavanetti <carlos-pavanetti@users.noreply.github.com>
2016-02-07 21:36:13 -0200

    Update nucleotide-count_spec.lua
    Adding local specifier to variables that don't need to be global. A good practice in Lua.

--- exercises/nucleotide-count/nucleotide-count_spec.lua
+++ exercises/nucleotide-count/nucleotide-count_spec.lua
@@ -4,7 +4,7 @@ describe('nucleotide-count', function()
 
   it('has no nucleotides', function()
     local expected =  { A = 0, T = 0, C = 0, G = 0 }
-    dna = DNA:new('')
+    local dna = DNA:new('')
     local result = dna.nucleotideCounts
     assert.are.same(expected, result)
   end)
@@ -12,35 +12,35 @@ describe('nucleotide-count', function()
   it('has no adenosine', function()
     local dna = DNA:new('')
     local expected = 0
-    result = dna:count('A')
+    local result = dna:count('A')
     assert.are.same(expected, result)
  end)
 
   it('repetitive cytosine gets counts', function()
     local dna = DNA:new('CCCCC')
     local expected = 5
-    result = dna:count('C')
+    local result = dna:count('C')
     assert.are.same(expected, result)
   end)
 
   it('repetitive sequence has only guanosine', function()
     local dna = DNA:new('GGGGGGGG')
-    expected = { A = 0, T = 0, C = 0, G = 8 }
-    result = dna.nucleotideCounts
+    local expected = { A = 0, T = 0, C = 0, G = 8 }
+    local result = dna.nucleotideCounts
     assert.are.same(expected, result)
   end)
 
   it('counts only thymine', function()
     local dna = DNA:new('GGGGTAACCCGG')
-    expected = 1
-    result = dna:count('T')
+    local expected = 1
+    local result = dna:count('T')
     assert.are.same(expected, result)
   end)
 
   it('counts a nucleotide only once', function()
     local dna = DNA:new('GGTTGG')
-    expected = 2
-    result = dna:count('T')
+    local expected = 2
+    local result = dna:count('T')
     assert.are.same(expected, result)
   end)
 
@@ -52,8 +52,8 @@ describe('nucleotide-count', function()
 
   it('counts all nucleotides', function()
     local dna = DNA:new('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
-    expected = { A = 20, T = 21, G = 17, C = 12 }
-    result = dna.nucleotideCounts
+    local expected = { A = 20, T = 21, G = 17, C = 12 }
+    local result = dna.nucleotideCounts
     assert.are.same(expected, result)
   end)
 
